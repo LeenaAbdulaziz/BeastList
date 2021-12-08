bdb97d77ba2b570ac91c6eab510940518f4112d6
linah_abdulaziz <linah.Abdulaziz@gmail.com>
2021-12-08 17:44:57 +0300

    BeastList

--- BeastList/ViewController.swift
+++ BeastList/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var tasks = [String]()
+    @IBOutlet weak var taskTextview: UITextField!
+    
+    @IBOutlet weak var tableview: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableview.dataSource = self
     }
 
-
+    @IBAction func preasedBeastButton(_ sender: UIButton) {
+        tasks.append(taskTextview.text!)
+        tableview.reloadData()
+        taskTextview.text = ""
+    }
+    
+   
 }
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableview.dequeueReusableCell(withIdentifier: "my cell", for: indexPath)
+        cell.textLabel?.text = tasks[indexPath.row]
+        
+        return cell
+    }
+    
+    
+    
+}
+
 
